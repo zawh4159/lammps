c098bc6c19697ca1da13604c5850b2853647e1fa
zawh4159 <zawh4159@colorado.edu>
2025-11-20 12:21:15 -0700

    bug fixes

--- src/BPM/bond_bpm_prony.cpp
+++ src/BPM/bond_bpm_prony.cpp
@@ -339,7 +339,7 @@ void BondBPMProny::compute(int eflag, int vflag)
     }
 
      if (n == 1) {
-        printf("k0: %f | r0: %f | r: %f | force: %f\n",k[type],r0,r,fbond); 
+        printf("k0: %f | r0: %f | r: %f | force: %f\n",k0[type],r0,r,fbond); 
      }
     // rate-dependent part of bond force
     // Loop through Maxwell elements
